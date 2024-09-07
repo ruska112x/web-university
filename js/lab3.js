fabb0544ca02acc19e5827912007559ecb02a076
Ruslan Karabalin <karabalin112@gmail.com>
2024-09-07 20:55:02 +0600

    feat: complete lab 3

--- js/lab3.js
+++ js/lab3.js
@@ -46,4 +46,46 @@ printResult(3, () => {
     }
 });
 
+printResult(4, () => {
+    let str1 = "Alpha";
+    let str2 = "Beta";
+    console.log(`Строка 1: ${str1}`);
+    console.log(`Строка 2: ${str2}`);
+    console.log(`Конкатенация: ${str1.concat(" & ").concat(str2)}`);
+    console.log(`Длинная строка: ${str1.length > str2.length ? str1 : str2}`);
+});
 
+printResult(5, () => {
+    let x = randomInteger(1, 100);
+    let y = randomInteger(1, 100);
+    console.log(`Число 1: ${x}`);
+    console.log(`Число 2: ${y}`);
+    let xFirst = Number(x.toString()[0]);
+    let yFirst = Number(y.toString()[0]);
+    console.log(`Первые числа ${xFirst === yFirst ? "" : "не "}совпадают`);
+    console.log(`Первые число без остатка ${x % y === 0 ? "" : "не "}делится без остатка`);
+    console.log(`Большее число: ${x > y ? x : y}`);
+});
+
+printResult(6, () => {
+    let x = randomInteger(1, 100);
+    let y = randomInteger(1, 100);
+    console.log(`Число 1: ${x}`);
+    console.log(`Число 2: ${y}`);
+    console.log(`Остаток от деления первого на второе: ${x % y}`);
+    console.log(`Сумма чисел: ${x + y}`);
+    console.log(`Произведение чисел: ${x * y}`);
+});
+
+printResult(7, () => {
+    let x = randomInteger(1, 50);
+    let y = randomInteger(1, 50);
+    let z = randomInteger(1, 50);
+    console.log(`Число 1: ${x}`);
+    console.log(`Число 2: ${y}`);
+    console.log(`Число 3: ${z}`);
+    console.log(`Среднее арифметическое: ${(x + y + z) / 3}`);
+    console.log(`Сумма квадратов: ${(x * x) + (y * y) + (z * z)}`);
+    console.log(`Наибольшее из чисел: ${Math.max(x, y, z)}`);
+    console.log(`Наименьшее из чисел: ${Math.min(x, y, z)}`);
+});
