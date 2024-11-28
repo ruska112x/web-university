800590d36f8fb2070459eb12ab384da699e73fe7
Ruslan Karabalin <karabalin112@gmail.com>
2024-11-28 11:52:57 +0600

    feat: add lab17

---
+++ lab17/main.js
@@ -0,0 +1,26 @@
+function predict() {
+    const answers = {
+        positive: [
+            "Да, без сомнений!",
+            "Все будет отлично!",
+            "Ты на правильном пути!"
+        ],
+        neutral: [
+            "Возможно...",
+            "Не могу точно сказать.",
+            "Все может измениться."
+        ],
+        negative: [
+            "Скорее всего, нет.",
+            "Это маловероятно.",
+            "Не ждите этого."
+        ]
+    };
+
+    const categories = ['positive', 'neutral', 'negative'];
+    const randomCategory = categories[Math.floor(Math.random() * categories.length)];
+
+    const randomAnswer = answers[randomCategory][Math.floor(Math.random() * answers[randomCategory].length)];
+
+    document.getElementById('answer').innerText = randomAnswer;
+}
